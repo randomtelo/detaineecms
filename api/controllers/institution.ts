c0b5a55795a40234272201b27bcee5e6bf3f0169
randomtelo <randomtelo@yandex.ru>
2021-01-29 20:39:11 +0300

    change InstitutionController

--- api/controllers/institution.ts
+++ api/controllers/institution.ts
@@ -1,19 +1,32 @@
 import InstitutionModel from '../models/InstitutionModel';
 
 async function getInstitutions(ctx: any) {
-    
+    const query =  await InstitutionModel.GetInstitutions().then( data => { return JSON.stringify(data) }, error => {
+        console.log('Err: ' + error);
+    });
+    ctx.body = '{"array":' + query + '}';
 }
 
 async function createInstitution(ctx: any) {
     
+    const query =  await InstitutionModel.CreateInstitution(ctx.request.body).then( data => { return JSON.stringify(data) }, error => {
+        console.log('Err: ' + error);
+    });
+    ctx.body = '{"array":' + query + '}';
 }
 
 async function updateInstitution(ctx: any) {
-    
+    const query =  await InstitutionModel.UpdateInstitution(ctx.params.id, ctx.request.body).then( data => { return JSON.stringify(data) }, error => {
+        console.log('Err: ' + error);
+    });
+    ctx.body = '{"array":' + query + '}';
 }
 
 async function deleteInstitution(ctx: any) {
-    
+    const query =  await InstitutionModel.DeleteInstitution(ctx.params.id).then( data => { return JSON.stringify(data) }, error => {
+        console.log('Err: ' + error);
+    });
+    ctx.body = '{"array":' + query + '}';
 }
 
 export = {
